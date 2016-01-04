c4cf8d439d7519f2f26c0bd65022c2c08c10ed6e
Aaron Edwards <KaiReptilon@ln-mowifi-02-10.1.30.191.net.bton.ac.uk>
2016-01-04 11:54:15 +0000

    Changes to the element_buffer

--- src/CubeAsset.cc
+++ src/CubeAsset.cc
@@ -17,6 +17,16 @@ CubeAsset::CubeAsset() {
   GLuint element_buffer []  {
     0, 1, 2
     , 1, 3, 2
+    , 0, 1, 4
+    , 1, 4, 5
+    , 4, 5, 6
+    , 5, 6, 7
+    , 0, 2, 4
+    , 2, 4, 6
+    , 2, 3, 6
+    , 3, 6, 7
+    , 1, 3, 5
+    , 3, 5, 7
   };
 
   // Transfer buffers to the GPU
